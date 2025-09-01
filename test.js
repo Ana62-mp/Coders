3977f3b3a0d7081a90f645b3f650e6cdb8340e2d
Ana62-mp <ana.moposita84@gmail.com>
2025-08-31 22:23:46 -0500

    ejercicio Saludo completado y con estilos

--- test.js
+++ test.js
@@ -4,10 +4,16 @@ saludar=function()
     let nombre
     let cmpApellido;
     let apellido
+    let cmpResultado;
+
     cmpNombre = document.getElementById("txtNombre");
     cmpApellido = document.getElementById("txtApellido");
     apellido=cmpApellido.value;
     nombre=cmpNombre.value;
     saludo=generarSaludo(nombre, apellido);
     console.log(saludo);
+
+    cmpResultado = document.getElementById('lblResultado');
+    cmpResultado.innerText = saludo;
+
 }
