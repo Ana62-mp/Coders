86d2fa47421b1483a90e05f7a23568f0f32c4546
Deyvid23r4 <deyvidcarrera10122007dc@gmail.com>
2025-08-31 21:39:26 -0500

    cracion de archivo test.html y revicion de funcionamiento

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ saludar=function()
     let apellido
     cmpNombre = document.getElementById("txtNombre");
     cmpApellido = document.getElementById("txtApellido");
-    nombre=cmpApellido.value;
-    apellido=cmpNombre.value;
+    apellido=cmpApellido.value;
+    nombre=cmpNombre.value;
     saludo=generarSaludo(nombre, apellido);
     console.log(saludo);
 }
